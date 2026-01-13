2a261025fddbebb1ebc73cbcad993bf658744adf
SearedBacon <2260910@flourbluffschools.org>
2026-01-13 12:10:37 -0600

    Added particles and changed audio to not play when dying

--- Player/player.gd
+++ Player/player.gd
@@ -6,44 +6,74 @@ class_name Player
 var transitioning:=false
 @onready var death: AudioStreamPlayer = $Death
 @onready var success: AudioStreamPlayer = $Success
-@onready var rocket_audio: AudioStreamPlayer3D = $RockerAudio
+@onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+@onready var main_booster: GPUParticles3D = $MainBooster
+@onready var right_turn: GPUParticles3D = $RightTurn
+@onready var left_turn: GPUParticles3D = $LeftTurn
+var yes:=0
 
 func _process(delta: float) -> void:
 	if !transitioning:
 		if Input.is_action_pressed("boost"):
+			main_booster.emitting=true
 			apply_central_force(basis.y*delta*thrust)
 			if not rocket_audio.is_playing():
 				rocket_audio.play()
 		else:
 			rocket_audio.stop()
+			main_booster.emitting=false
+	
 		if Input.is_action_pressed("rotate_left"):
 			apply_torque(Vector3(0.0,0.0,delta*torque))
+			left_turn.emitting=true
+		else:
+			left_turn.emitting=false
+			
 		if Input.is_action_pressed("rotate_right"):
 			apply_torque(Vector3(0.0,0.0,-delta*torque))
+			right_turn.emitting=true
+		else:
+			right_turn.emitting=false
 
 
 func _on_body_entered(body: Node) -> void:
 	if "Goal" in body.get_groups():
 		print("You Win")
 		if body.file_path!=null:
+			rocket_audio.stop()
+			main_booster.emitting=false
+			right_turn.emitting=false
+			left_turn.emitting=false
 			level_complete(body.file_path)
 		else:
 			print("No next level found!")
 
 	if "Bad" in body.get_groups():
+		rocket_audio.stop()
+		main_booster.emitting=false
+		right_turn.emitting=false
+		left_turn.emitting=false
 		crash_sequence()
 
 func crash_sequence()->void:
-	death.play()
+	if yes==0:
+		death.play()
 	print("KABOOM")
 	transitioning=true
-	await get_tree().create_timer(1).timeout
+	yes=1
+	await get_tree().create_timer(2.5).timeout
 	transitioning=false
 	get_tree().reload_current_scene.call_deferred()
+	yes=0
+	rocket_audio.stop()
+	main_booster.emitting=false
 	
 func level_complete(next_level_file)->void:
-	success.play()
-	if transitioning==false:
-		await get_tree().create_timer(1).timeout
-		get_tree().change_scene_to_file(next_level_file)
-		transitioning=false
+	if yes==0:
+		success.play()
+	transitioning=true
+	yes=1
+	await get_tree().create_timer(2.5).timeout
+	get_tree().change_scene_to_file(next_level_file)
+	transitioning=false
+	yes=0
